6edb828295c9fc3b4b9653af37d647a6024730c7
Magua61 <110238665+Magua61@users.noreply.github.com>
2025-07-17 13:56:49 +0800

    Initial commit

---
+++ MoneyGer/MoneyGerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyGerApp.swift
+//  MoneyGer
+//
+//  Created by Carl Brian Sacatani on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MoneyGerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
